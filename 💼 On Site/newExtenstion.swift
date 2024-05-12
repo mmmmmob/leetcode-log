ac4c9f03e2bdef7e807d5ec1069de5263a1386da
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-05-13 00:17:57 +0700

    feat: add Date extension

--- 💼 On Site/newExtenstion.swift
+++ 💼 On Site/newExtenstion.swift
@@ -1,18 +1,86 @@
 import Foundation
 
-/*
 extension Date {
-  var day: Int 
-  var nameOfDay: String
-  var month: Int
-  var nameOfTheMonth: String
-  var year: Int
-
-  func numberOfDaysBetween(date1: Int, date2: Int) -> Int {
     
-  }
+    var day: Int {
+        let calendar = Calendar.current
+        return calendar.component(.day, from: self)
+    }
+    
+    var nameOfDay: String {
+        let calendar = Calendar.current
+        switch calendar.component(.day, from: self) {
+        case 2:
+            return "Monday"
+        case 3:
+            return "Tuesday"
+        case 4:
+            return "Wednesday"
+        case 5:
+            return "Thursday"
+        case 6:
+            return "Friday"
+        case 7:
+            return "Saturday"
+        default:
+            return "Sunday"
+        }
+    }
+    
+    var month: Int {
+        let calendar = Calendar.current
+        return calendar.component(.month, from: self)
+    }
+    
+    var nameOfTheMonth: String {
+        let calendar = Calendar.current
+        switch calendar.component(.month, from: self) {
+        case 1:
+            return "January"
+        case 2:
+            return "February"
+        case 3:
+            return "March"
+        case 4:
+            return "April"
+        case 5:
+            return "May"
+        case 6:
+            return "June"
+        case 7:
+            return "July"
+        case 8:
+            return "August"
+        case 9:
+            return "September"
+        case 10:
+            return "October"
+        case 11:
+            return "November"
+        default:
+            return "December"
+        }
+    }
+    
+    var year: Int {
+        let calendar = Calendar.current
+        return calendar.component(.year, from: self)
+    }
+    
+    
+    static func numberOfDaysBetween(date1: Date, date2: Date) -> Int {
+        return abs(date1.day - date2.day)
+    }
+    
 }
-*/
+
+let date = Date()
+print(date.day)
+print(date.nameOfDay)
+print(date.month)
+print(date.nameOfTheMonth)
+print(date.year)
+print(Date.numberOfDaysBetween(date1: date, date2: Date(timeIntervalSinceNow: 691_200)))
 
 extension String {
     func toDate() -> Date? {
